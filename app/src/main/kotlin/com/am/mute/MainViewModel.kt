0dd81ce3d026b538863be748f349f2fc6930b23c
Adhish Mathur <adhish.mathur@gmail.com>
2023-12-02 16:13:21 +0530

    Added check for last known values

---
+++ app/src/main/kotlin/com/am/mute/MainViewModel.kt
@@ -0,0 +1,32 @@
+package com.am.mute
+
+import android.content.SharedPreferences
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    fun updateLastVolumeStream(
+        sharedPreferences: SharedPreferences,
+        currentAudioValue: Int,
+        currentNotificationValue: Int
+    ) = CoroutineScope(Dispatchers.IO).launch{
+        sharedPreferences.edit().apply {
+            putInt(CURRENT_AUDIO_VOLUME, currentAudioValue)
+            putInt(CURRENT_NOTIFICATION_VOLUME, currentNotificationValue)
+        }.apply()
+    }
+
+
+    fun getLastVolumeStream(sharedPreferences: SharedPreferences): Pair<Int, Int> = Pair(
+        sharedPreferences.getInt(
+            CURRENT_AUDIO_VOLUME, 0
+        ), sharedPreferences.getInt(CURRENT_NOTIFICATION_VOLUME, 0)
+    )
+
+    private companion object {
+        const val CURRENT_AUDIO_VOLUME = "current_audio_volume"
+        const val CURRENT_NOTIFICATION_VOLUME = "current_notification_volume"
+    }
+}
--- app/src/main/kotlin/com/am/mute/MuteActivity.kt
+++ app/src/main/kotlin/com/am/mute/MuteActivity.kt
@@ -1,29 +1,36 @@
 package com.am.mute
 
-import android.app.Activity
 import android.content.Context
 import android.media.AudioManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 /**
  * @author Adhish
  */
-class MuteActivity : Activity() {
+class MuteActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
         val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        val maxRingVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING)
-        val maxNotificationVolume =
-            audioManager.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION)
-        audioManager.takeIf {
-            it.getStreamVolume(AudioManager.STREAM_RING) > 0 || it.getStreamVolume(AudioManager.STREAM_NOTIFICATION) > 0
-        }?.run {
-            setStreamVolume(AudioManager.STREAM_RING, 0, 0)
-            setStreamVolume(AudioManager.STREAM_NOTIFICATION, 0, 0)
-        } ?: run {
-            audioManager.setStreamVolume(AudioManager.STREAM_RING, maxRingVolume, 0)
-            audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, maxNotificationVolume, 0)
+        val currentRingVolume = audioManager.getStreamVolume(AudioManager.STREAM_RING)
+        val currentNotificationVolume = audioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION)
+        audioManager.apply {
+            if (currentRingVolume > 0) {
+                viewModel.updateLastVolumeStream(sharedPreferences, currentRingVolume, currentNotificationVolume)
+                setStreamVolume(AudioManager.STREAM_RING, 0, 0)
+                setStreamVolume(AudioManager.STREAM_NOTIFICATION, 0, 0)
+            } else {
+                viewModel.getLastVolumeStream(sharedPreferences).apply {
+                    setStreamVolume(AudioManager.STREAM_RING, first, 0)
+                    setStreamVolume(AudioManager.STREAM_NOTIFICATION, second, 0)
+                }
+            }
         }
         finishAndRemoveTask()
     }
