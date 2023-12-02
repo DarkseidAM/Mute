3e0dde04b224fb728a3c523774e5a01a8aab8cce
Adhish Mathur <adhish.mathur@gmail.com>
2023-12-02 16:32:55 +0530

    Upgraded version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.am.mute"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1000
-        versionName = "1.0.0"
+        versionCode = 1001
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
